f5c90aa246d87041ed34a07ce6f614522c19d3ba
Bolu Adetifa <68053657+Bolu1@users.noreply.github.com>
2022-02-13 16:11:13 +0100

    Frr

--- pages/api/create-stripe-session/index.js
+++ pages/api/create-stripe-session/index.js
@@ -1,19 +1,16 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 async function CreateStripeSession(req, res) {
   const { item } = req.body;
-  console.log(req.body);
-  console.log("cHEHE");
-  console.log(item);
 
   const redirectURL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://ecommercestuff.vercel.app/success";
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://stripe-checkout-next-js-demo.vercel.app';
 
   const transformedItem = {
     price_data: {
-      currency: "usd",
+      currency: 'usd',
       product_data: {
         name: item.name,
       },
@@ -22,18 +19,14 @@ async function CreateStripeSession(req, res) {
     description: item.description,
     quantity: item.quantity,
   };
-  console.log("trans")
-  console.log(transformedItem)
+
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
+    payment_method_types: ['card'],
     line_items: [transformedItem],
-    mode: "payment",
-    success_url: redirectURL + "?status=success",
-    cancel_url: redirectURL + "?status=cancel",
+    mode: 'payment',
+    success_url: redirectURL + '?status=success',
+    cancel_url: redirectURL + '?status=cancel',
   });
-  console.log("should be")
-  console.log(session)
+
   res.json({ id: session.id });
 }
-
-export default CreateStripeSession;
