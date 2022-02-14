89aee8d9dab3653d058ff75c38749fad414760ec
Bolu1 <boluadetifa@gmail.com>
2022-02-14 13:48:25 +0100

    testing

--- pages/api/create-stripe-session/index.js
+++ pages/api/create-stripe-session/index.js
@@ -1,12 +1,12 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 async function CreateStripeSession(req, res) {
   const { item } = req.body;
 
   const redirectURL =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://stripe-checkout-next-js-demo.vercel.app';
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://stripe-checkout-next-js-demo.vercel.app";
 
   // const transformedItem = {
   //   price_data: {
@@ -19,23 +19,25 @@ async function CreateStripeSession(req, res) {
   //   description: item.description,
   //   quantity: item.quantity,
   // };
-  const amount =  item.price * 100
-
+  const amount = item.price * 100;
+  const CURRENCY = 'usd'
+  console.log(amount);
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    submit_type: "donate",
+    payment_method_types: ["card"],
     line_items: [
       {
-        name: 'Custom amount donation',
-        amount: amount,
-        currency: 'usd',
+        name: "Pay",
+        amount: 40,
+        currency: CURRENCY,
         quantity: 1,
       },
     ],
-    mode: 'payment',
-    success_url: redirectURL + '?status=success',
-    cancel_url: redirectURL + '?status=cancel',
+    success_url: `http://localhost:3000/success`,
+    cancel_url: `http://localhost:3000/cancel`,
   });
-
+  console.log("hehh");
+  console.log(amount);
   res.json({ id: session.id });
 }
 
